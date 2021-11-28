09dc5e8fc9adcacfc1218619b15cb1bc6cfff09a
zabuter <sfrady20@gmail.com>
2021-11-28 12:35:20 -0500

    make template

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import ContentProvider from './components/Content';
 import Router from './components/Router';
 
 const App = () => {
   return (
-    <ContentProvider>
-      <div className="fixed left-0 top-0 w-full h-full flex flex-col justify-center items-center select-none">
-        <Router />
-      </div>
-    </ContentProvider>
+    <div className="fixed left-0 top-0 w-full h-full flex flex-col justify-center items-center">
+      <Router />
+    </div>
   );
 };
 
--- src/renderer/components/Router/index.tsx
+++ src/renderer/components/Router/index.tsx
@@ -1,112 +1,13 @@
-import {
-  AnimatePresence,
-  motion,
-  MotionValue,
-  useTransform,
-} from 'framer-motion';
-import { map, times } from 'lodash';
-import React, { useRef } from 'react';
-import { Routes, Route, useLocation } from 'react-router';
-import { HashRouter } from 'react-router-dom';
-import { toGraphicUrl, useContent } from '../Content';
-import SplashPage from '/@/routes/1_Splash';
-import MenuPage from '/@/routes/2_Menu';
-import HolidayPage from '/@/routes/3_Holiday';
-
-const levels = 6;
-
-const TransitionBar = (props: {
-  index: number;
-  transitionValue: MotionValue<number>;
-}) => {
-  const { index, transitionValue } = props;
-  const dx = useTransform(transitionValue, (tv) => {
-    let ntv = tv;
-    //loop
-    ntv = ((ntv + 2) % 2) - 1;
-    //scale
-    ntv *= -3000 * ((ntv > 0 ? index : levels - index) / levels) - 500;
-    return ntv;
-  });
-
-  return (
-    <div className="flex-1 relative">
-      <svg className="absolute -top-1000px">
-        <defs>
-          <filter id={`transition-${index}`}>
-            <motion.feOffset dy={dx} />
-          </filter>
-        </defs>
-      </svg>
-      <motion.div
-        className="absolute bg-white inset-0 bg-cover"
-        style={{
-          backgroundImage: `url(${toGraphicUrl('/TransitionBar.png')})`,
-          filter: `url(#transition-${index})`,
-        }}
-      />
-    </div>
-  );
-};
-
-const AnimatedRoutes = () => {
-  const location = useLocation();
-
-  const transitionValue = useRef(new MotionValue(0)).current;
-  const transitionDuration = useContent(
-    (c) => c.config.transitionDuration || 0.8,
-  );
-
-  return (
-    <>
-      <div className="fixed inset-0 flex flex-col z-100 pointer-events-none">
-        {map(times(levels), (i) => (
-          <TransitionBar key={i} index={i} transitionValue={transitionValue} />
-        ))}
-      </div>
-      <AnimatePresence initial={false} exitBeforeEnter>
-        <motion.div
-          key={location.pathname}
-          variants={{
-            animate: {
-              x1: 0,
-              transition: {
-                duration: transitionDuration,
-                ease: 'easeIn',
-              },
-            },
-            exit: {
-              x1: 1,
-              transition: {
-                duration: transitionDuration,
-                ease: 'easeOut',
-              },
-            },
-          }}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          className="contents"
-          onUpdate={(latest) => {
-            const val = latest.x1 as number;
-            transitionValue.set(val);
-          }}
-        >
-          <Routes location={location}>
-            <Route path="/" element={<SplashPage />} />
-            <Route path="/menu" element={<MenuPage />} />
-            <Route path="/holiday/:holidayIndex" element={<HolidayPage />} />
-          </Routes>
-        </motion.div>
-      </AnimatePresence>
-    </>
-  );
-};
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from '/@/routes/Home';
 
 const Router = () => {
   return (
     <HashRouter>
-      <AnimatedRoutes />
+      <Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
     </HashRouter>
   );
 };
